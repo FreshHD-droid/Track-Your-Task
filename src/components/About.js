0c64c2b37ba1b6181113ff7a2daccb6cdbf3facc
Marcu George <robert.marcu03@gmail.com>
2021-12-10 00:19:27 +0200

    1.1 Commit
    Nothing really special

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import {Link} from 'react-router-dom'
+
+const About = () => {
+    return (
+        <div>
+            <h4>Version 1.0.1</h4>
+            <h4>by Mark George Robert</h4>
+            <Link to="/">Go Back</Link>
+        </div>
+    )
+}
+
+export default About
